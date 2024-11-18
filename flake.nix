c9762b4f912e314ba504f3ce38bf2d08a005b861
jobs62 <droper@analphabet.me>
2024-11-18 13:39:45 +0100

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -30,7 +30,7 @@
 
         inherit (pkgs) lib;
 
-        craneLib = crane.lib.${system};
+        craneLib = crane.mkLib pkgs;
         src = craneLib.cleanCargoSource (craneLib.path ./.);
 
         commonArgs = {
